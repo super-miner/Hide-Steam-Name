2a517e794e8fc769681536d117d923aa7206ed79
super-miner <theredmugproductions@gmail.com>
2024-11-22 21:50:57 -0800

    Initial commit

---
+++ HideSteamName/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using BepInEx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace HideSteamName {
+    [BepInPlugin(PluginInfo.PLUGIN_ID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin {
+        private void Awake() {
+            SceneManager.activeSceneChanged += (from, to) => {
+                if (to.name == "Title Screen") {
+                    HideSteamName(to);
+                }
+            };
+        }
+
+        private void HideSteamName(Scene titleScreenScene) {
+            Transform titleScreenTransform = GetTransformByName("Title Screen", titleScreenScene.GetRootGameObjects());
+            Transform uiTransform = GetChildByName("UI", titleScreenTransform);
+            Transform steamNameTransform = GetChildByName("Steam Name", uiTransform);
+            
+            steamNameTransform.gameObject.SetActive(false);
+        }
+
+        private Transform GetTransformByName(string name, GameObject[] objects) {
+            return objects.First(gameObject => gameObject.name == name).transform;
+        }
+
+        private Transform GetChildByName(string name, Transform parent) {
+            for (int i = 0; i < parent.childCount; i++) {
+                Transform child = parent.GetChild(i);
+
+                if (child.name == name) {
+                    return child;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ HideSteamName/PluginInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using HideSteamName;
+
+#region Assembly attributes
+
+[assembly: AssemblyVersion(PluginInfo.PLUGIN_VERSION)]
+[assembly: AssemblyTitle(PluginInfo.PLUGIN_NAME + " (" + PluginInfo.PLUGIN_ID + ")")]
+[assembly: AssemblyProduct(PluginInfo.PLUGIN_NAME)]
+
+#endregion
+
+namespace HideSteamName {
+    internal static class PluginInfo {
+        public const string PLUGIN_NAME = "Hide Steam Name";
+
+        public const string PLUGIN_ID = "flowndev.com.hidesteamname";
+
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
